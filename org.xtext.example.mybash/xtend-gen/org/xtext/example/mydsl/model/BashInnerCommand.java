5eb49d28468d5c0c79ee3a067724e3ed9d40c76d
qscdfg <qscdfg@163.com>
2016-05-20 09:07:39 +0800

    remove unused files

--- org.xtext.example.mybash/xtend-gen/org/xtext/example/mydsl/model/BashInnerCommand.java
+++
@@ -1,7 +0,0 @@
-package org.xtext.example.mydsl.model;
-
-import org.xtext.example.mydsl.myBash.impl.FunctionDefineImpl;
-
-@SuppressWarnings("all")
-public class BashInnerCommand extends FunctionDefineImpl {
-}
